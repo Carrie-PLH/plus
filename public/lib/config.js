718ff93faa3ca69dcc800cbff551f7b693f4c4f5
Carrie Schluter <carrie@schluters.com>
2025-08-25 18:15:00 -0400

    Add public/lib folder with Firebase config

---
+++ public/lib/config.js
@@ -0,0 +1,10 @@
+// Copy this to config.js and fill in your values
+// NEVER commit config.js to Git!
+export const firebaseConfig = {
+  apiKey: "AIzaSyDDmdy5dlbudx432p7PR8sV_7xVrc-u01o",
+  authDomain: "black-monitor-469420-h9.firebaseapp.com",
+  projectId: "black-monitor-469420-h9",
+  storageBucket: "black-monitor-469420-h9.firebasestorage.app",
+  messagingSenderId: "87660782331",
+  appId: "1:87660782331:web:5a50b3ff2833584ca2865e"
+};
